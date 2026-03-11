ba50fbc103082b5f8065b8a67582261ef3a3dda3
Akash72077 <sai.sai72077@gmail.com>
2026-03-12 00:33:22 +0530

    Support folder updated

---
+++ frontend/src/landing_page/support/SupportPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Hero from "./Hero";
+import CreateTicket from "./CreateTicket";
+const SupportPage = () => {
+  return <div>SupportPage</div>;
+};
+
+export default SupportPage;
